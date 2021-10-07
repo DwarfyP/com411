b3980e5099a0c6401ffec7d958fcf1d2c536ac02
Alex Mcdonald <5mcdoa36@solent.ac.uk>
2021-10-07 11:56:49 +0100

    Modulo Operator

--- basics/week 2/Elif.py
+++ basics/week 2/Elif.py
@@ -2,11 +2,11 @@ print("in which direction should i paint")
 direction = input()
 if direction == "up":
     print("up")
-elif direction == "right"
+elif direction == "right":
     print("right")
-elif direction == "down"
+elif direction == "down":
     print("down")
-elif direction == "left"
+elif direction == "left":
     print("left")
 print("")
 print(f"i am painting in {direction} direction")
