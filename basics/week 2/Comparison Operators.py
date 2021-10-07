59751a72fba6df9a71421d6772a36d67e49ef5c1
Alex Mcdonald <5mcdoa36@solent.ac.uk>
2021-10-07 12:15:19 +0100

    Comparason Oporators

---
+++ basics/week 2/Comparison Operators.py
@@ -0,0 +1,10 @@
+print("enter first number")
+first_number = input()
+print(f"{first_number}")
+print("enter second number")
+second_number = input()
+print(f"{second_number}")
+if first_number > second_number:
+    print("First number is bigger")
+else:
+    print("first number is equal or smaller")
---
+++ basics/week 2/Modulo_operator.py
@@ -0,0 +1,6 @@
+print("please enter a whole number")
+number = int (input())
+if  (number % 2) == 0:
+    print(f"{number} is Even number")
+else:
+    print(f"{number} is Odd number")
