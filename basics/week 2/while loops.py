881ea6e33e500b27e6acba004caa09aeb6417b46
Alex Mcdonald <5mcdoa36@solent.ac.uk>
2021-10-14 12:46:03 +0100

    end week 3

--- basics/week 2/while loops.py
+++ basics/week 2/while loops.py
@@ -3,3 +3,4 @@ print (f"we have {cables} installed how many do you want to remove?")
 while cables > 0:
     cables = cables - 1
     print(f"removed {cables}")
+    
---
+++ basics/week 2/while_ascii.py
@@ -0,0 +1,6 @@
+print("how many bars should be charged?")
+needed = input()
+charge = 0
+while charge > "needed":
+    charge = charge + 1
+    print("battery fully charged")
