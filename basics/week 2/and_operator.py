4c9efd743388fd24a6e8ed6f2975a7294c2b0b15
Alex Mcdonald <5mcdoa36@solent.ac.uk>
2021-10-14 12:26:47 +0100

    while_loops

---
+++ basics/week 2/and_operator.py
@@ -0,0 +1,8 @@
+print("what did i hear?")
+hear = input()
+print("what did i see?")
+see = input()
+if (hear == "grrr") and (see == "two red eyes"):
+    print("there is a scary creature i need to get outa here")
+else:
+    print("i see a creature but it looks okay")
---
+++ basics/week 2/while loops.py
@@ -0,0 +1,5 @@
+cables = 6
+print (f"we have {cables} installed how many do you want to remove?")
+while cables > 0:
+    cables = cables - 1
+    print(f"removed {cables}")
